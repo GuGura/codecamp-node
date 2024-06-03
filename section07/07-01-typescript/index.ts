04bce56c325370b16aa00ad373788ade1bd8a709
pjy <wodus331@vanillacode.net>
2024-06-03 10:21:41 +0900

    07-02 typescript

--- section07/07-01-typescript/index.ts
+++ section07/07-01-typescript/index.ts
@@ -36,3 +36,39 @@ undefined
 let fff:number[] = [1,2,3,4,5,"인냥하세요"];
 let ggg:string[] = ['철수','영희','훈이',10];
 let hhh: (string | number)[] = ['철수','영희','훈이',10];
+
+// 객체타입
+interface IProfile {
+    name: string
+    age: number |string
+    school: string
+    hobby?:string
+}
+const profile: IProfile= {
+    name: "철수",
+    age: 8,
+    school: "킹재연아카데미"
+}
+profile.name ="훈이"; // 타입 추론으로는 이것만 가능
+profile.age="8살";
+profile.hobby = "수영"
+
+// 함수타입 => 어디서 몇번이든 포출 가능하므로, 타입추론 할 수 없음(반드시 타입명시 필요!!)
+function add(num1:number, num2:number, unit:string): string{
+    return num1 + num2 + unit
+}
+
+const result = add(1000,2000,"원") // 결과의 리턴 타입도 예측 가능!!
+add('철수',2000,"원")
+
+
+const  add2 = (num1:number, num2:number, unit:string): string =>{
+    return num1 + num2 + unit
+}
+
+const result2 = add(1000,2000,"원") // 결과의 리턴 타입도 예측 가능!!
+
+// any타입
+let qqq:any = "철수" // 자바스크립트와 동일
+qqq = 123
+qqq = true
